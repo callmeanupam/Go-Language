d534bb59c01825b90d5263828fa9059101099868
Anupam Shakya <theanupamshakya@gmail.com>
2024-06-21 17:55:26 +0530

    Updated

--- 02variables/main.go
+++ 02variables/main.go
@@ -5,11 +5,18 @@ import "fmt"
 func main(){
 	// fmt.Println("variables");
 	var username string = "variables" // string
-	var age int = 23 // number or integer
-	var condition bool = true
-	var fll float64 = 100.100
-	fmt.Println(username) // printing string
-	fmt.Println(age) // printing age
-	fmt.Println(condition) // condition
-	fmt.Print(fll) // float
+	fmt.Println(username)
+	fmt.Printf("Variable is of type: %T \n", username)
+	
+	var isLoggedIn bool = false
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is of type : %T \n", isLoggedIn)
+
+	// var age int = 23 // number or integer
+	// var condition bool = true
+	// var fll float64 = 100.100
+	
+	// fmt.Println(age) // printing age
+	// fmt.Println(condition) // condition
+	// fmt.Print(fll) // float
 }
