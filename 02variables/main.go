ccb573c67b5fff12c33c1d8b0fc73248d100367b
Anupam Shakya <theanupamshakya@gmail.com>
2024-06-21 12:20:51 +0530

    Variables

---
+++ 02variables/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	// fmt.Println("variables");
+	var username string = "variables" // string
+	var age int = 23 // number or integer
+	var condition bool = true
+	var fll float64 = 100.100
+	fmt.Println(username) // printing string
+	fmt.Println(age) // printing age
+	fmt.Println(condition) // condition
+	fmt.Print(fll) // float
+}
