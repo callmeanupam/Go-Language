6813b970c2d7ba5f5aca43799cab6be9c0f690de
Anupam Shakya <theanupamshakya@gmail.com>
2024-06-22 19:11:05 +0530

    comma and err syntax

--- 03userinput/main.go
+++ 03userinput/main.go
@@ -1,8 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main(){
 	welcome:= "welcome to user input"
 	fmt.Println(welcome)
+
+	reader:= bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the rating for out pizza:")
+
+	// hello := "to here"
+	// fmt.Println(hello)
+
+	// comma om and err (error) ok syntax
+
+	input, _ := reader.ReadString('\n')
+	// input and hold an error
+	fmt.Println("Thanks for rating ", input)
+	fmt.Printf("Thanks for rating %T \n", input)
+
+	
+
 }
